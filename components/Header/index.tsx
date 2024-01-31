cd42c563f4f90df66a66f0cc04c34767df8d59c8
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-01-31 18:05:36 +0800

    Updated HTML Header with Header Container

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import styles from './header.module.css';
 import { IconMoon } from '../Icons/Moon';
 
-
 export const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1>TODO</h1>
+    <header className={styles.header__container}>
+      <div className={styles.header}>
+        <h1>TODO</h1>
 
-      <div>
-        <button>
-          <IconMoon />
-        </button>
+        <div>
+          <button>
+            <IconMoon />
+          </button>
+        </div>
       </div>
     </header>
   );
